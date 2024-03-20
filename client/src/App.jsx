81a296adb4985439b41bc1a60bbefc7c1293c063
Jai-Solania-29 <jaideep2912@gmail.com>
2024-03-20 14:32:46 +0530

    My Account Components Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,8 @@ import LoginPage from './Pages/LoginPage';
 import Layout from './Layout';
 import RegisterPage from './Pages/RegisterPage';
 import axios from 'axios';
-import { UserContextProvider } from "./UserContext";
+import {UserContextProvider} from "./UserContext";
+import AccountPage from './Pages/AccountPage';
 
 axios.defaults.baseURL = 'http://localhost:4000';
 axios.defaults.withCredentials = true;
@@ -20,6 +21,7 @@ function App() {
           <Route index element={<IndexPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/account/:subpage?" element={<AccountPage/>} />
         </Route>
       </Routes>
     </UserContextProvider>
