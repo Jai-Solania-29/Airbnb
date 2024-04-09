20c8eee11cd28d39193783f5f896b8d032f7f460
Jai-Solania-29 <jaideep2912@gmail.com>
2024-04-10 01:46:57 +0530

    Photo Uploading Functionality

--- api/index.js
+++ api/index.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const User = require('./models/User.js');
 const cookieParser = require('cookie-parser')
 const imageDownloader = require('image-downloader');
+const multer = require('multer');
+const fs = require('fs');
+
 require('dotenv').config();
 const app = express();
 
@@ -14,6 +17,7 @@ const jwtSecret = 'hfgshjgsdhgfhhidfg';
 
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname +'/models/uploads'));
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173',
@@ -81,12 +85,26 @@ app.post('/logout', (req, res) => {
 
 app.post('/upload-by-link', async (req,res) => {
     const {link} = req.body;
-    const newName = Date.now() + '.jpg';
+    const newName = 'photo' + Date.now() + '.jpg';
     await imageDownloader.image({
         url: link,
         dest: __dirname + '/models/uploads/' + newName,
     });
-    res.json(__dirname + '/uploads/' + newName);
-})
+    res.json(newName);
+});
+
+const photosMiddleware = multer({dest:'uploads/'});
+app.post('/upload', photosMiddleware.array('photos', 100), (req,res) => {
+    const uploadedFiles = [];
+    for (let i = 0; i < req.files.length; i++) {
+        const {path, originalname} = req.files[i];
+        const parts = originalname.split('.');
+        const ext = parts[parts.length -1];
+        const newPath = path + '.' + ext;
+        fs.renameSync(path, newPath);
+        uploadedFiles.push(newPath.replace('uploads/', ''));
+    }
+    res.json(uploadedFiles);
+});
 
 app.listen(4000);
